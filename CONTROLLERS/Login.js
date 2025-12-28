1e83627d11fd90c332f05e77eb5f804e330e6c27
LIL-MANIAC-001 <127343644+MrLloyd808@users.noreply.github.com>
2025-12-28 12:51:48 +0200

    fixing cookie errors

--- CONTROLLERS/Login.js
+++ CONTROLLERS/Login.js
@@ -41,8 +41,8 @@ const LoginUser = async (req, res) => {
    res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
-    maxAge: 24 * 60 * 60 * 1000
+    sameSite: "none",
+    maxAge: 7 * 24 * 60 * 60 * 1000
    })
 
   res.status(200).json({
