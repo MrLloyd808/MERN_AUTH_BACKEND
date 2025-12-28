18449583c8d155a9bd8e259bd8ca9d762e58c5f7
LIL-MANIAC-001 <127343644+MrLloyd808@users.noreply.github.com>
2025-12-28 16:24:06 +0200

    / error fix on cors

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.DB_URI)
 
 
 app.use(cors({
-    origin: "https://mernauthpractice.netlify.app/",
+    origin: "https://mernauthpractice.netlify.app",
     credentials: true
 }))
 
