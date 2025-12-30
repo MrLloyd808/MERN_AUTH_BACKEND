a96041aa469e6e028f52992a18e151ff7c17a273
LIL-MANIAC-001 <127343644+MrLloyd808@users.noreply.github.com>
2025-12-30 11:42:19 +0200

    changed stuff

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.use(cors({
     credentials: true
 }))
 
-app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
 app.use(express.static(path.join(__dirname, "static")))
 app.use('/auth', UserRouter)
 app.use("/api", PageRouter)
