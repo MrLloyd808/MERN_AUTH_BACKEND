e93e378ce6d393d1e80164a6ed0f7c6703f9c14b
LIL-MANIAC-001 <127343644+MrLloyd808@users.noreply.github.com>
2026-01-03 20:31:37 +0200

    added delete route

---
+++ CONTROLLERS/DeletePost.js
@@ -0,0 +1,26 @@
+const PostSchema = require("../MODELS/PostSchema")
+
+const DeletePost = async (req, res) => {
+    
+    const { id } = req.params
+
+    try {
+        const thisPost = await PostSchema.findById(id)
+        if (!thisPost) return res.status(404).json({
+           success: false,
+           message: "this post does not exist"
+        })
+
+        await thisPost.deleteOne()
+
+        res.sendStatus(204)
+
+    } catch (error) {
+        res.status(500).json({
+         success: false,
+         message: "Internal server error: " + error.message
+        })
+    }
+}
+
+module.exports = DeletePost
